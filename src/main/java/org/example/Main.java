b999851c8445949510fc2b4cfd5aa1b9ef48eb83
asrytikov <asrytikov@gmail.com>
2023-11-15 11:53:03 +0400

    Lesson29

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,20 @@
-package org.example;public class Main {
+package org.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Person person = context.getBean(Person.class);
+
+        Dog dog = context.getBean(Dog.class);
+
+        System.out.println(person.getName());
+        System.out.println(dog.getName());
+        System.out.println(person.getDog());
+
+
     }
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,11 +1,18 @@
 package org.example;
 
-import org.springframework.stereotype.Component;
 
-@Component
 public class Person {
 
     private String name;
+    private Dog dog;
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
 
     public String getName() {
         return name;
